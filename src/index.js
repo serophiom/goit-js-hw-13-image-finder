2f3326b3a95d33800331635d9899df649131a540
serophiom <serophiom@gmail.com>
2021-06-06 09:59:56 +0300

    добавляет поиск изображений

--- src/index.js
+++ src/index.js
@@ -5,6 +5,60 @@ import imageCardTpl from '../src/templates/image-cards.hbs';
 // import '@pnotify/core/dist/PNotify.css';
 // import '@pnotify/core/dist/BrightTheme.css';
 
+const debounce = require('lodash.debounce');
+const refs = {
+    searchForm: document.querySelector('.search-form'),
+    inputRef: document.querySelector('.input'),
+    galleryRef: document.querySelector('.gallery'),
+    photCardRef: document.querySelector('.photo-card'),
+    loadMoreBtn: document.querySelector('.btn-more'),
+}
+
+refs.searchForm.addEventListener('submit', onSearch);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+
+function onSearch(event) {
+        event.preventDefault();
+        // const inputRefValue = refs.inputRef.value.trim();
+        const inputRefValue = event.currentTarget.elements.query.value;
+        // const inputRefValueTrim = inputRefValue.trim();
+        API.apiService(inputRefValue, 2).then(markUpSearchResult);
+    }
+
+function onLoadMore () {
+    
+}
+
+function markUpSearchResult(image) {
+    refs.galleryRef.innerHTML = '';
+    const markUp = imageCardTpl(image);
+    refs.galleryRef.innerHTML = markUp;
+    
+
+
+
+    // if (country.length === 1) {
+    //     const markUp = countryCardTpl(country);
+    //      refs.cardRef.innerHTML = markUp;
+    // } else
+        
+    //     if (country.length >= 2 && country.length <= 10) {
+    //         const markUpCountriesList = countriesListCardTpl(country);
+    //         refs.cardRef.innerHTML = markUpCountriesList;
+    //            return;
+    //        } else
+                
+    //          if (country.length > 10) {
+    //             error({
+    //                 text: 'Too many matches found. Please enter a more specific query!',
+    //                 delay: 3000,
+    //                 });
+    //         }
+    return;
+}
+
+
+
 // const options = {
 //     headers: {
 //         Authotization: '',
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,23 +1,25 @@
 // import { error } from '@pnotify/core';
 // import '@pnotify/core/dist/PNotify.css';
 // import '@pnotify/core/dist/BrightTheme.css';
-// const BASE_URL = ''
+const BASE_URL = 'https://pixabay.com/api/?image_type=photo&orientation=horizontal&'
+const AUTH_KEY = '21951044-669b58be8946ea9fe70fb579e';
 
-function apiService (searchInput, pageNumber, yourKey) {
-    return fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchInput}&page=${pageNumber}&per_page=12&key=${yourKey}`).then(response => {
-        // if (!response.ok) {
-        // response.status === 404
-        // error({
-        //     text: 'Can`t find country with this name. Please try another one.',
-        //     delay: 3000,
-        //   })
-    // }
+function apiService (searchInput, pageNumber) {
+    return fetch(`${BASE_URL}&q=${searchInput}&page=${pageNumber}&per_page=12&key=${AUTH_KEY}`).then(response => {
+        if (!response.ok) {
+        response.status === 404
+            error({
+                text: 'Can`t find country with this name. Please try another one.',
+                delay: 3000,
+            })
+        }
+        // console.log(response.json());
         return response.json();
+        
     });
 }
 
 export default { apiService };
-
 
 // import { error } from '@pnotify/core';
 // import '@pnotify/core/dist/PNotify.css';
